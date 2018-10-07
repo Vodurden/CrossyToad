8855e40b32c2a78b6c239ec87d89d7126ca563f6
Jake Woods <jake@jakewoods.net>
2018-10-07 19:45:09 +1100

    Update to use nixpkgs-18.09
    Since 18.09 has all the versions we want we don't have to use a weird mixture
    of unstable and stable. Hooray!

--- shell.nix
+++ shell.nix
@@ -6,61 +6,42 @@ let
     url = "https://github.com/nixos/nixpkgs-channels/archive/${pinnedVersion.rev}.tar.gz";
     sha256 = pinnedVersion.sha256;
   };
-  pkgs = import nixpkgs {
-    config = {
-      packageOverrides = oldPkgs: {
-        glibc = pkgsUnstable.glibc;
-      };
-    };
-  };
+  pkgs = import nixpkgs {};
 
-  haskellPackagesStable = pkgs.haskell.packages.${compiler}.override {
-    overrides = haskellNew: haskellOld: rec {
-      mkDerivation = args: haskellOld.mkDerivation (args // {
-        enableLibraryProfiling = true;
-      });
-    };
-  };
-
-  # We want to be able to pull some packages from _the future_!
-  unstablePinnedVersion = builtins.fromJSON (builtins.readFile ./nix/nixpkgs-unstable-version.json);
-  nixpkgsUnstable = builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs-channels/archive/${unstablePinnedVersion.rev}.tar.gz";
-    sha256 = unstablePinnedVersion.sha256;
-  };
-  pkgsUnstable = import nixpkgsUnstable {};
-
-  f = import ./default.nix;
+  crossyToad = import ./default.nix;
 
   # We're using pkgsUnstable as it has ecstasy 2.0. We can replace this with nixos-18.09 when
   # it releases.
-  haskellPackages = pkgsUnstable.haskell.packages.${compiler}.override {
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = haskellNew: haskellOld: rec {
+      # sdl2 tests fail with an error related to an XDG environment variable
+      sdl2 = pkgs.haskell.lib.dontCheck haskellOld.sdl2;
+
       # OpenGL segfaults when we try to haddock it. It also fails to link when we use
       # a version of OpenGL from a nixpkgs later then our NixOS install. It seems to be unable
       # to find certain OpenGL symbols.
       #
       # To work around this we use the stable versions of the OpenGL libraries.
-      GLUT = pkgs.haskell.lib.dontHaddock haskellPackagesStable.GLUT;
-      OpenGL = pkgs.haskell.lib.dontHaddock haskellPackagesStable.OpenGL;
-      OpenGLRaw = pkgs.haskell.lib.dontHaddock haskellPackagesStable.OpenGLRaw;
+      # GLUT = pkgs.haskell.lib.dontHaddock haskellPackagesStable.GLUT;
+      # OpenGL = pkgs.haskell.lib.dontHaddock haskellPackagesStable.OpenGL;
+      # OpenGLRaw = pkgs.haskell.lib.dontHaddock haskellPackagesStable.OpenGLRaw;
 
       # Since we're using some stable stuff, sometimes we need to force other libraries
       # to fall back to their stable version to prevent cabal from complaining.
-      kan-extensions = haskellPackagesStable.kan-extensions;
-      lens = haskellPackagesStable.lens;
-      sdl2 = haskellPackagesStable.sdl2;
-      sdl2-ttf = haskellPackagesStable.sdl2-ttf;
-      hpack = haskellPackagesStable.hpack;
+      # kan-extensions = haskellPackagesStable.kan-extensions;
+      # lens = haskellPackagesStable.lens;
+      # sdl2 = haskellPackagesStable.sdl2;
+      # sdl2-ttf = haskellPackagesStable.sdl2-ttf;
+      # hpack = haskellPackagesStable.hpack;
 
       # these tests take _forever_
-      linear = pkgsUnstable.haskell.lib.dontCheck haskellPackagesStable.linear;
-      happy = pkgsUnstable.haskell.lib.dontCheck haskellOld.happy;
+      # linear = pkgsUnstable.haskell.lib.dontCheck haskellOld.linear;
+      # happy = pkgsUnstable.haskell.lib.dontCheck haskellOld.happy;
     };
   };
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (haskellPackages.callPackage crossyToad {});
 in
   if pkgs.lib.inNixShell then drv.env else drv
