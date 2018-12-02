ff3e60cda8449e5beb04d7e9ac01e4d157f8e5d4
Jake Woods <jake@jakewoods.net>
2018-12-02 13:58:43 +1100

    Put dante's build directory under `dist-newstyle`

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 
 (
  (haskell-mode
-  (dante-repl-command-line . ("nix-shell" "--pure" "--run" (concat "cabal new-repl " dante-target " --builddir=dist/dante"))))
+  (dante-repl-command-line . ("nix-shell" "--pure" "--run" (concat "cabal new-repl " dante-target " --builddir=dist-newstyle/dante"))))
  ("src" (haskell-mode (dante-target . "lib:crossy-toad")))
  ("executable" (haskell-mode (dante-target . "exe:crossy-toad")))
  ("test" (haskell-mode (dante-target . "spec")))
