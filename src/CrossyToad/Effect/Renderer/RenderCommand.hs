8132a1cc6b111337013b35edf3a604831a71f1b7
Jake Woods <jake@jakewoods.net>
2018-12-07 07:25:32 +1100

    Add render tests to TitleSpec

--- src/CrossyToad/Effect/Renderer/RenderCommand.hs
+++ src/CrossyToad/Effect/Renderer/RenderCommand.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module CrossyToad.Effect.Renderer.RenderCommand where
 
+import Control.Lens
 import Data.Degrees (Degrees)
 import Data.Text (Text)
 
 import CrossyToad.Effect.Renderer.FontAsset (FontAsset)
-import CrossyToad.Effect.Renderer.ImageAsset (ImageAsset)
+import CrossyToad.Effect.Renderer.ImageAsset (ImageAsset, AsImageAsset(..))
 import CrossyToad.Effect.Renderer.PixelClip
 import CrossyToad.Effect.Renderer.PixelPosition
 import CrossyToad.Effect.Renderer.RGBAColour
@@ -28,3 +31,10 @@ data RenderCommand
              !(Maybe ScreenClip)
              !RGBAColour
              !Text
+  deriving (Show, Eq)
+
+makeClassy ''RenderCommand
+makeClassyPrisms ''RenderCommand
+
+instance AsImageAsset RenderCommand where
+  _ImageAsset = _RenderCommand . _ImageAsset
--- test/CrossyToad/Scene/Title/TitleSpec.hs
+++ test/CrossyToad/Scene/Title/TitleSpec.hs
@@ -1,16 +1,23 @@
 module CrossyToad.Scene.Title.TitleSpec where
 
+import           Control.Lens
 import           Test.Tasty.Hspec
 
+import           CrossyToad.Effect.Renderer.RenderCommand
 import qualified CrossyToad.Scene.Scene as Scene
 import qualified CrossyToad.Scene.Title.Intent as Intent
 import           CrossyToad.Scene.Title.Title
 
 spec_Scene_Title :: Spec
-spec_Scene_Title =
+spec_Scene_Title = do
   describe "stepIntent" $ do
     it "should start the game when the intent is StartGame" $
       stepIntent Intent.StartGame Scene.Title `shouldBe` Scene.Game
 
     it "should quit the game when the intent is Quit" $
       stepIntent Intent.Quit Scene.Title `shouldBe` Scene.Quit
+
+  describe "render" $ do
+    it "should draw ' CROSSY TOAD ' on the screen" $ do
+      let texts = (^? _DrawText . _6) <$> render
+      texts `shouldContain` [(Just " CROSSY TOAD ")]
